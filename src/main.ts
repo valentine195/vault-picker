d7edb3cdf97558ccd180a3f33b363d637cef959a
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-02-28 12:34:53 -0500

    init

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,41 @@
 import "./main.css";
 
-import { Plugin } from "obsidian";
+import { Platform, Plugin } from "obsidian";
 import { PluginSettings } from "./@types";
-
-const DEFAULT_SETTINGS: PluginSettings = {};
 
 export default class MyPlugin extends Plugin {
     settings: PluginSettings;
     async onload() {
-        await this.loadSettings();
+        this.app.workspace.onLayoutReady(async () => {
+            if (Platform.isMobile) {
+                localStorage.removeItem("mobile-selected-vault");
+            } else {
+                const fs = require("fs");
+                const path = require("path");
+                const { remote } = require("electron");
+                const { app } = remote;
+                console.log(app.getPath("userData"));
+
+                const data = fs.readFileSync(
+                    path.join(app.getPath("userData"), "obsidian.json"),
+                    { encoding: "utf8" }
+                );
+
+                const parsed = JSON.parse(data ?? {});
+                if (!parsed || !parsed.vaults) return;
+
+                if (parsed?.vaults) {
+                    for (const vault in parsed.vaults) {
+                        delete parsed.vaults[vault].open;
+                    }
+                }
+                fs.writeFileSync(
+                    path.join(app.getPath("userData"), "obsidian.json"),
+                    JSON.stringify(parsed)
+                );
+            }
+        });
     }
 
     onunload() {}
-
-    async loadSettings() {
-        this.settings = Object.assign(
-            {},
-            DEFAULT_SETTINGS,
-            await this.loadData()
-        );
-    }
-
-    async saveSettings() {
-        await this.saveData(this.settings);
-    }
 }
