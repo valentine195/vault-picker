c0a3cb82b92b8e7075b1956291418fe3d98e9f15
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-02-28 12:58:10 -0500

    feat: added settings

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,1 +1,4 @@
-export interface PluginSettings {}
+export interface PluginSettings {
+    mobile: boolean;
+    desktop: boolean;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,38 @@
 import "./main.css";
 
-import { Platform, Plugin } from "obsidian";
+import {
+    FileSystemAdapter,
+    Notice,
+    Platform,
+    Plugin,
+    PluginSettingTab,
+    Setting
+} from "obsidian";
 import { PluginSettings } from "./@types";
 
-export default class MyPlugin extends Plugin {
+const DEFAULT_SETTINGS: PluginSettings = {
+    mobile: false,
+    desktop: false
+};
+
+export default class VaultPicker extends Plugin {
     settings: PluginSettings;
     async onload() {
+        this.settings = { ...DEFAULT_SETTINGS, ...(await this.loadData()) };
+        this.addSettingTab(new VaultPickerSettings(this));
         this.app.workspace.onLayoutReady(async () => {
-            if (Platform.isMobile) {
+            this.tryToReset();
+        });
+    }
+    tryToReset() {
+        try {
+            if (Platform.isMobile && this.settings.mobile) {
                 localStorage.removeItem("mobile-selected-vault");
-            } else {
+            } else if (this.settings.desktop) {
                 const fs = require("fs");
                 const path = require("path");
                 const { remote } = require("electron");
                 const { app } = remote;
-                console.log(app.getPath("userData"));
 
                 const data = fs.readFileSync(
                     path.join(app.getPath("userData"), "obsidian.json"),
@@ -26,7 +44,14 @@ export default class MyPlugin extends Plugin {
 
                 if (parsed?.vaults) {
                     for (const vault in parsed.vaults) {
-                        delete parsed.vaults[vault].open;
+                        if (
+                            parsed.vaults[vault].path ==
+                            (
+                                this.app.vault.adapter as FileSystemAdapter
+                            ).getBasePath()
+                        ) {
+                            delete parsed.vaults[vault].open;
+                        }
                     }
                 }
                 fs.writeFileSync(
@@ -34,8 +59,48 @@ export default class MyPlugin extends Plugin {
                     JSON.stringify(parsed)
                 );
             }
-        });
+        } catch (e) {
+            console.error(e);
+            new Notice(
+                "Vault Picker: There was an issue removing this vault from the stored open vaults."
+            );
+        }
     }
-
     onunload() {}
+}
+
+class VaultPickerSettings extends PluginSettingTab {
+    constructor(public plugin: VaultPicker) {
+        super(plugin.app, plugin);
+    }
+    display() {
+        this.containerEl.empty();
+        this.containerEl.createEl("h3", { text: "Vault Picker" });
+
+        this.containerEl.createSpan({
+            cls: "setting-item",
+            text: "Turning these settings on is effective immediately - the next time Obsidian opens, the vault picker will be shown. Turning them off, however, will still show the vault picker on the next restart."
+        });
+
+        new Setting(this.containerEl)
+            .setName("Desktop")
+            .setDesc("Vault picker will always open on desktop.")
+            .addToggle((t) =>
+                t.setValue(this.plugin.settings.desktop).onChange(async (v) => {
+                    this.plugin.settings.desktop = v;
+                    await this.plugin.saveData(this.plugin.settings);
+                    if (!Platform.isMobile) this.plugin.tryToReset();
+                })
+            );
+        new Setting(this.containerEl)
+            .setName("Mobile")
+            .setDesc("Vault picker will always open on mobile.")
+            .addToggle((t) =>
+                t.setValue(this.plugin.settings.mobile).onChange(async (v) => {
+                    this.plugin.settings.mobile = v;
+                    await this.plugin.saveData(this.plugin.settings);
+                    if (Platform.isMobile) this.plugin.tryToReset();
+                })
+            );
+    }
 }
